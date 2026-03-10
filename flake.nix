4c006931c376600b6d63feffb7d36b443d8be50e
Mladen Branković <mladen.brankovic@golog.ch>
2026-03-10 16:24:46 +0100

    feat: set up nix env

---
+++ flake.nix
@@ -0,0 +1,66 @@
+{
+  description = "A toolbox for language construction";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+
+    rust = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs:
+    inputs.utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = inputs.nixpkgs.legacyPackages.${system}.appendOverlays [
+          inputs.rust.overlays.default
+        ];
+        rust = rec {
+          toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+          platform = pkgs.makeRustPlatform {
+            cargo = toolchain;
+            rustc = toolchain;
+          };
+        };
+        treefmt = inputs.treefmt-nix.lib.evalModule pkgs {
+          projectRootFile = "flake.nix";
+          programs = {
+            alejandra.enable = true;
+            rustfmt.enable = true;
+          };
+        };
+      in {
+        packages = let
+          manifest = pkgs.lib.importTOML ./Cargo.toml;
+        in rec {
+          default = cabra;
+
+          cabra = rust.platform.buildRustPackage {
+            inherit (manifest.package) name version;
+
+            cargoLock.lockFile = ./Cargo.lock;
+            src = pkgs.lib.cleanSource ./.;
+
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+            ];
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          name = "cabra";
+
+          buildInputs = with pkgs; [
+            rust.toolchain
+            pkg-config
+          ];
+        };
+
+        formatter = treefmt.config.build.wrapper;
+      }
+    );
+}
