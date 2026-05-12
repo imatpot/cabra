711413d88a5d232afd687c3b484beede64906704
Mladen Branković <mladen.brankovic@golog.ch>
2026-05-12 19:07:29 +0200

    feat: human vs agent

--- src/main.rs
+++ src/main.rs
@@ -1,95 +1,74 @@
 #![allow(dead_code)] // TODO: remove, it's just nice to quiet down rust-analyzer
 
+use std::io::{self, Write, stdin};
+
+use rand::{Rng, rng};
+
+use crate::mcts::policy::computation::Iterative;
+use crate::{
+	caminos::{
+		placement::LEGAL_PLACEMENTS,
+		state::{GameState, Player},
+	},
+	mcts::agent::{MctsAgent, MctsAgentConfig},
+};
+
 pub mod caminos;
 pub mod mcts;
 pub mod util;
 
-use std::io::Write;
-use std::io::{self};
-
-use crate::{
-	caminos::{
-		file::{ReadFromPath, WriteToPath},
-		placement::PlacementRefs,
-		state::{GameResult, GameState, Player},
-	},
-	mcts::{
-		agent::{MctsAgent, MctsAgentConfig},
-		policy::{
-			computation::Iterative,
-			rollout::{PlacementBias, RolloutPolicy},
-		},
-	},
-	util::ansi,
-};
-
 fn main() {
-	let mut alice = MctsAgent::new(MctsAgentConfig {
-		computational_limit: Box::new(Iterative { iterations: 50_000 }),
-		rollout_policy: RolloutPolicy::unseeded(&[PlacementBias::TouchingOwn(1e10)]),
-		..MctsAgentConfig::default()
-	});
-
-	let mut bob = MctsAgent::new(MctsAgentConfig {
+	let mut agent = MctsAgent::new(MctsAgentConfig {
 		computational_limit: Box::new(Iterative { iterations: 50_000 }),
 		..MctsAgentConfig::default()
 	});
 
 	let mut state = GameState::EMPTY;
-	let mut placements: PlacementRefs = Vec::new();
 
-	loop {
-		if let Some(result) = state.determine_winner() {
-			match result {
-				GameResult::StrongWin(Player::A) => {
-					println!("{}Player A wins strongly!{}", ansi::GREEN, ansi::RESET)
-				}
-				GameResult::WeakWin(Player::A) => {
-					println!("{}Player A wins weakly!{}", ansi::BLUE, ansi::RESET)
-				}
+	let human = if rng().next_u64() % 2 == 0 {
+		Player::A
+	} else {
+		Player::B
+	};
 
-				GameResult::StrongWin(Player::B) => {
-					println!("{}Player B wins strongly!{}", ansi::RED, ansi::RESET)
-				}
-				GameResult::WeakWin(Player::B) => {
-					println!("{}Player B wins weakly!{}", ansi::RED, ansi::RESET)
-				}
-
-				GameResult::Draw => println!("{}It's a draw!{}", ansi::YELLOW, ansi::RESET),
-			}
-
-			break;
-		}
-
-		let best_move = match state.next_player() {
-			Player::A => alice.search_best_placement(&state),
-			Player::B => bob.search_best_placement(&state),
-		};
-
-		if let Some(placement) = best_move {
-			println!("Player {} places {}", state.next_player(), placement);
-			state.apply_placement(placement);
-			placements.push(placement);
-			alice.prune(&state.as_node_id());
-			bob.prune(&state.as_node_id());
+	while state.determine_winner().is_none() {
+		if state.next_player() == human {
+			human_turn(&mut state);
 		} else {
-			println!("Game over! No valid move found");
-			break;
+			agent_turn(&mut state, &mut agent);
 		}
 
-		println!("{state}");
+		println!("{}", state);
+	}
+}
 
-		if std::env::args().any(|arg| arg == "--wait") {
-			print!("Press Enter to continue...");
+fn human_turn(state: &mut GameState) {
+	print!("Your turn: ");
+	io::stdout().flush().unwrap();
+
+	let mut input = String::new();
+	let mut placement = None;
+
+	while placement.is_none() {
+		io::stdin().read_line(&mut input).unwrap();
+		placement = LEGAL_PLACEMENTS
+			.of_all_without_overlap_without_floating(
+				&(state.players[0].occupancy | state.players[1].occupancy),
+			)
+			.find(|p| p.notation == input.trim());
+
+		if placement.is_none() {
+			print!("Invalid placement {}, try again: ", input.trim());
 			io::stdout().flush().unwrap();
-			let mut input = String::new();
-			io::stdin().read_line(&mut input).unwrap();
-			println!();
+			input.clear();
 		}
 	}
 
-	let path = "result.caminos";
-	placements.write_to_path(path, true).unwrap();
-	let loaded_state = GameState::read_from_path(path).unwrap();
-	println!("\nLoaded game state from {}:\n{}", path, loaded_state);
+	state.apply_placement(placement.unwrap());
+}
+
+fn agent_turn(state: &mut GameState, agent: &mut MctsAgent) {
+	let placement = agent.search_best_placement(state).unwrap();
+	state.apply_placement(placement);
+	println!("Agent plays: {}", placement.notation);
 }
